c1291044d7fca0aa53e5f48b2e373b21fe9a26b0
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-07-29 23:46:17 +0700

    feat: Wrapped with page layout

--- src/setup/PageRouter/PageRouter.tsx
+++ src/setup/PageRouter/PageRouter.tsx
@@ -4,16 +4,22 @@ import { HomePage } from 'pages/HomePage'
 import { TodoDetailPage } from 'pages/TodoDetailPage'
 
 import { paths } from './routes'
+import { PageLayout } from 'layouts/PageLayout'
 
 export const PageRouter = () => {
   const router = createBrowserRouter([
     {
-      path: paths.home,
-      element: <HomePage />,
-    },
-    {
-      path: paths.todoDetail,
-      element: <TodoDetailPage />,
+      element: <PageLayout />,
+      children: [
+        {
+          path: paths.home,
+          element: <HomePage />,
+        },
+        {
+          path: paths.todoDetail,
+          element: <TodoDetailPage />,
+        },
+      ],
     },
   ])
 
